b262ed264fcfd60821d7684db21ab52ebad9add0
marvelousajuzie <Chizurummarvelous14@gmail.com>
2025-10-20 13:03:22 +0100

    fff

--- client/pages/Landing.tsx
+++ client/pages/Landing.tsx
@@ -208,16 +208,16 @@ export default function Landing() {
       ].map((item, idx) => (
         <div key={idx} className="text-left">
           {/* Icon */}
-          <div className="w-16 h-16 rounded-xl bg-blue-100 flex items-center justify-center mb-4">
+          <div className="w-16 h-16  items-center justify-center mb-4">
             <img 
               src={item.icon} 
               alt={item.title} 
-              className="w-10 h-10 object-contain"
+              className="w-15 h-15 object-contain"
             />
           </div>
           
           {/* Title */}
-          <h3 className="text-2xl font-bold text-black mb-2">
+          <h3 className="text-4xl font-bold text-black mb-2">
             {item.step}: {item.title}
           </h3>
           
