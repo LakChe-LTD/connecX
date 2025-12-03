a39efe4b64e98ada47c614374653e0f3ed27d16c
marvelousajuzie <Chizurummarvelous14@gmail.com>
2025-12-03 18:50:10 +0100

    implemented the end user profile ui completed

--- client/pages/EndUsersDashboard/Profile.tsx
+++ client/pages/EndUsersDashboard/Profile.tsx
@@ -404,7 +404,7 @@ export default function ProfileSettings() {
                     />
                   </button>
                 </div>
-
+                    //connection events
                 <div className="flex items-start justify-between py-4">
                   <div className="flex-1">
                     <h3 className={`text-base font-semibold mb-1 ${theme === 'dark' ? 'text-white' : 'text-gray-900'}`}>
